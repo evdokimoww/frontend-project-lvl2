8b7cbed114784530885f02d24beb9bc27c642bd4
evdokimoww <La15mCrSP1>
2021-11-30 10:09:53 +0300

    normalize bug fix

--- src/supportFunctions.js
+++ src/supportFunctions.js
@@ -1,11 +1,7 @@
 export const isObject = (element) => typeof element === 'object';
 
 export const normalizeValue = (val) => {
-  if (val === 'true'
-    || val === 'false'
-    || val === 'null'
-    || typeof val === 'boolean'
-    || typeof val === 'number') return val;
-  if (typeof val === 'string') return `'${val}'`;
-  return '[complex value]';
+  if (typeof val === 'object') return '[complex value]';
+  if (typeof val === 'string' && val !== 'true' && val !== 'false' && val !== 'null') return `'${val}'`;
+  return val;
 };
