786a831d6169fd7e4d2c71046a498ae9d0ab2936
evdokimoww <La15mCrSP1>
2021-12-07 14:37:27 +0300

    revisions #1 formatter normalizeValue function

--- formatters/plain.js
+++ formatters/plain.js
@@ -1,4 +1,11 @@
-import { normalizeValue } from '../src/supportFunctions.js';
+const normalizeValue = (val) => {
+  if (val === 'true' || val === true) return true;
+  if (val === 'false' || val === false) return false;
+  if (val === 'null' || val === null) return null;
+  if (!Number.isNaN(val * 1) && val !== '') return Number(val);
+  if (typeof val === 'object') return '[complex value]';
+  return `'${val}'`;
+};
 
 const plain = (ast) => {
   const iter = (currentValue, path) => {
--- src/supportFunctions.js
+++
@@ -1,9 +0,0 @@
-// eslint-disable-next-line import/prefer-default-export
-export const normalizeValue = (val) => {
-  if (val === 'true' || val === true) return true;
-  if (val === 'false' || val === false) return false;
-  if (val === 'null' || val === null) return null;
-  if (!Number.isNaN(val * 1) && val !== '') return Number(val);
-  if (typeof val === 'object') return '[complex value]';
-  return `'${val}'`;
-};
